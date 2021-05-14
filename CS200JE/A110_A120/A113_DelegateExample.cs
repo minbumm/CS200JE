6578bbd2d7ae74a0a2fa5f5bb86339868f5f4193
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-05-15 08:17:29 +0900

    A112

---
+++ CS200JE/A110_A120/A113_DelegateExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A110_A120
+{
+    class A113_DelegateExample
+    {
+        delegate bool MemberTest(int a);
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 3, 5, 4, 2, 6, 4, 6, 8, 54, 23, 4, 6, 4 };
+
+            Console.WriteLine(count:(4));
+        }
+
+        static int Count(int[] a, MemberTest testMethd) 
+        {
+            int cnt = 0;
+            fo
+        }
+    }
+}
