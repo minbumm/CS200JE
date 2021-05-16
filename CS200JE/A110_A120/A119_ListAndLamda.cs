51dbbf4e996b6e6cefa51c9fade7ee1aad79e10d
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-05-17 00:23:41 +0900

    A119_A120

---
+++ CS200JE/A110_A120/A119_ListAndLamda.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A110_A120
+{
+    class A119_ListAndLamda
+    {
+        static void Main(string[] args)
+        {
+            List<String> myList = new List<string> { 
+                "mouse", "cow", "tiger", "rabbit", "dragon", "snake"
+            };
+
+            bool n = myList.Exists(s => s.Contains("x"));
+            Console.WriteLine("이름에 'x'를 포함하는 동물이 있나요 :" + n);
+
+            Console.WriteLine("이름이 3글자인 첫번째 동물 : ");
+            string name = myList.Find(s => s.Length == 3);
+            Console.WriteLine(name);
+
+            Console.WriteLine("6글자 이상의 동물들:  ");
+            List<string> longName = myList.FindAll(s => s.Length > 5);
+            foreach (var item in longName)
+            {
+                Console.WriteLine(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("대문자로 변환");
+            List<string> capList = myList.ConvertAll(s => s.ToUpper());
+            foreach (var item in capList)
+            {
+                Console.WriteLine(item + " ");
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
---
+++ CS200JE/A110_A120/A120_LinqBasic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A110_A120
+{
+    class A120_LinqBasic
+    {
+        static void Main(string[] args)
+        {
+            List<int> data = new List<int> { 123, 45, 12, 89, 456, 1, 4, 74, 46 };
+            List<int> lstSortedEven = new List<int>();
+
+            foreach (var item in data)
+            {
+                if (item % 2 == 0)
+                {
+                    lstSortedEven.Add(item);
+                }
+            }
+            lstSortedEven.Sort();
+
+            Console.WriteLine("Using foreach");
+            foreach (var item in lstSortedEven)
+            {
+                Console.WriteLine(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
