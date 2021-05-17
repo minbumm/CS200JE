e0a3d830971a2d96b46c21a4aac5ff3082b6fbe5
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-05-18 08:38:27 +0900

    A080

---
+++ CS200JE/A080_A089/A080_RecursiveFactorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A080_A089
+{
+    class A080_RecursiveFactorial
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("m!을 계산합니다. m를 입력하세요");
+            double m = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0}! = {1}", m, Fact(m));
+        }
+
+        private static double Fact(double x) 
+        {
+            if (x == 1)
+            {
+                return 1;
+            }
+            else 
+            {
+                return x * Fact(x - 1);
+            }
+        }
+    }
+}
