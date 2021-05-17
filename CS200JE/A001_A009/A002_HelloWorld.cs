950d16f3d8a6cb6303ed851d9183d2222a5e82cc
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-05-18 00:10:09 +0900

    테스트

---
+++ CS200JE/A001_A009/A002_HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A001_A009
+{
+    class A002_HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
