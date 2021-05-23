91820cb51b7fa9e6312af841e7ddf2da57b505d9
minbumm <68464294+minbumm@users.noreply.github.com>
2021-05-24 08:27:07 +0900

    A077

---
+++ CS200JE/A070_A079/A077_Power.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A070_A079
+{
+    class A077_Power
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 20; i++)
+                Console.WriteLine("2 ^ {0,2} = {1, 7}", i, Power(2, i));
+        }
+
+        private static int Power(int m, int n)
+        {
+            int p = 1;
+            for (int i = 1; i <= m; i++)
+                p *= n;
+            return p;
+        }
+    }
+}
