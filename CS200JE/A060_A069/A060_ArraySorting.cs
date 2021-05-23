99207ba1f4439717e199d645a4a2fbf45cd12a54
minbumm <68464294+minbumm@users.noreply.github.com>
2021-05-24 08:39:27 +0900

    A060

---
+++ CS200JE/A060_A069/A060_ArraySorting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A060_A069
+{
+    class A060_ArraySorting
+    {
+        static void Main(string[] args)
+        {
+            string[] name = { "Mouse", "Cow", "Tiger", "Rabbit", "Dragon", "Snake", "Horse" };
+            PrintArray("Before Sor: ", name);
+
+            Array.Sort(name);
+            PrintArray("After Sort: ", name);
+
+            Array.Reverse(name);
+            PrintArray("After Reverse: ", name);
+        }
+
+
+        private static void PrintArray(string s, string[] name) 
+        {
+            Console.WriteLine(s);
+            foreach (var n in name)
+            {
+                Console.WriteLine("{0}", n);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
