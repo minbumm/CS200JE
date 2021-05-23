03f94a1a54a66526ca1d7033e7de39376c3553df
minbumm <68464294+minbumm@users.noreply.github.com>
2021-05-24 08:33:56 +0900

    A079

---
+++ CS200JE/A070_A079/A079_RecursivePower.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A070_A079
+{
+    class A079_RecursivePower
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Povwe(x,y)를 계산합니다.");
+            Console.Write(" x를 입력하세요!: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write(" y를 입력하세요: ");
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine(" {0}^{1} = {2}", x, y, Power(x, y));
+
+        }
+
+
+        private static double Power(double x, double y) 
+        {
+            if (y == 0)
+            {
+                return 1;
+            }
+            else 
+            {
+                return x * Power(x, y - 1);
+            }
+        }
+    }
+}
