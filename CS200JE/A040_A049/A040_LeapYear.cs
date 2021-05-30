be2f12afa6e674044e36fd5774e635ef64858d89
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-05-30 22:00:45 +0900

    A040

---
+++ CS200JE/A040_A049/A040_LeapYear.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A040_A049
+{
+    class A040_LeapYear
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+
+            //for (int year = 100; year <= 4000; year += 100)
+            //{
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                Console.WriteLine("{0}년은 윤년", year);
+            else
+                Console.WriteLine("{0}년은 평년", year);
+
+            if (DateTime.IsLeapYear(year))
+                Console.WriteLine("{0}년은 윤년", year);
+            else
+                Console.WriteLine("{0}년은 평년", year);
+            //}
+        }
+    }
+}
