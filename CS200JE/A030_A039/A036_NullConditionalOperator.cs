5b58b3b9dcba9ad39453501603e96d155793aab4
minbumm <68464294+minbumm@users.noreply.github.com>
2021-06-03 09:00:55 +0900

    A036

---
+++ CS200JE/A030_A039/A036_NullConditionalOperator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A030_A039
+{
+    class A036_NullConditionalOperator
+    {
+        static void Main(string[] args)
+        {
+            string animal = null;
+
+            Console.WriteLine("4글자 이상인 동물의 이름만 출력합니다.");
+            do
+            {
+                LongNameAnimal(animal);
+                Console.Write("동물이름: ");
+            } while ((animal = Console.ReadLine()) != "");
+        }
+        private static void LongNameAnimal(string animal) 
+        {
+            if (animal.Length >= 4)
+            {
+                Console.WriteLine(animal + " : " + animal.Length);
+            }
+        }
+    }
+}
