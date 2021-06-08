87dc6ac97a18af47e4b3bac169bb52490ae90edc
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-06-08 22:47:40 +0900

    A024

---
+++ CS200JE/A020_A029/A024_CompoundAssignment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A020_A029
+{
+    class A024_CompoundAssignment
+    {
+        static void Main(string[] args)
+        {
+            int x = 32;
+
+            Console.WriteLine(x += 2);
+            Console.WriteLine(x -= 8);
+            Console.WriteLine(x *= 3);
+            Console.WriteLine(x /= 2);
+            Console.WriteLine(x++);
+            Console.WriteLine(--x);
+        }
+    }
+}
