7204ac7ea1cb2d4938665ddf9746495a147bdc7a
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-06-10 00:57:18 +0900

    A006

---
+++ CS200JE/A001_A009/A006_Assignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A001_A009
+{
+    class A006_Assignment
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double x;
+
+            i = 5;
+            x = 3.141592;
+            Console.WriteLine("i = " + i + ", x = " + x);
+
+            x = i; // 암시적 형변환 from int to double
+            i = (int)x; // 캐스트가 필요함
+            Console.WriteLine("i = " + i + ", x = " + x);
+        }
+    }
+}
