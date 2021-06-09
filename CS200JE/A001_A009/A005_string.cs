12e0cbbbbf75dbf002e65fcf9ba6092eceb1381e
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-06-10 00:56:04 +0900

    A005

---
+++ CS200JE/A001_A009/A005_string.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A001_A009
+{
+    class A005_string
+    {
+        static void Main(string[] args)
+        {
+            string a = "hello";
+            string b = "h";
+
+            b += "ello";
+            Console.WriteLine(a == b);
+            Console.WriteLine("b = " + b);
+
+            int x = 10;
+            string c = b + '!' + " " + x;
+            Console.WriteLine("c = " + c);
+        }
+    }
+}
