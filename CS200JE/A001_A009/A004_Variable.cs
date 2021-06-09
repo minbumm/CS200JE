d1ea9fe8a8b9f0d6ae2b7510b12ae326b91533e5
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-06-10 00:55:15 +0900

    A004

---
+++ CS200JE/A001_A009/A004_Variable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A001_A009
+{
+    class A004_Variable
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.WriteLine("World!");
+            Console.Write("이름을 입력하세요: ");
+
+            string name = Console.ReadLine();
+            Console.Write("안녕하세요, ");
+            Console.Write(name);
+            Console.WriteLine("님!");
+        }
+    }
+}
