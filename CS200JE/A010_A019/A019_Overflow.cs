d68ae520b5432323701d27203c5be02e93050afe
minbumm <68464294+minbumm@users.noreply.github.com>
2021-06-22 14:37:30 +0900

    A019

--- CS200JE/A010_A019/A019_Overflow.cs
+++ CS200JE/A010_A019/A019_Overflow.cs
@@ -38,7 +38,50 @@ namespace CS200JE.A010_A019
 
             checked
             {
-                    
+                try
+                {
+                    y = x + 10;
+                }
+                catch (Exception e) 
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine("int.MaxValue + 10 = {0}", y);
+            Console.Read();
+
+            checked
+            {
+                try
+                {
+                    Console.WriteLine(y = x + 10);
+                    Console.WriteLine(y = x - 1);
+                    Console.WriteLine(y = x * 2);
+                    Console.WriteLine(y = x / 2);
+                    Console.WriteLine(y = x % 2);
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("OverflowExcetion 발생 ");
+                }
+            }
+
+            int value = 780000000;
+            checked
+            {
+                try
+                {
+                    // Square the original value.
+                    int square = value * value;
+                    Console.WriteLine("{0} ^ 2 = {1}", value, square);
+                }
+                catch (OverflowException)
+                {
+                    double square = Math.Pow(value, 2);
+                    Console.WriteLine("Exception: {0} > {1:E}.",
+                                      square, Int32.MaxValue);
+                }
             }
         }
     }
