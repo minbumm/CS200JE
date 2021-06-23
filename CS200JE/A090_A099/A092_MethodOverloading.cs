7ed8cc2be0d315bc674e04c6fee4dfd7c7fd44f4
minbumm <68464294+minbumm@users.noreply.github.com>
2021-06-23 13:25:17 +0900

    A092

---
+++ CS200JE/A090_A099/A092_MethodOverloading.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS200JE.A090_A099
+{
+    class A092_MethodOverloading
+    {
+        static void Main(string[] args)
+        {
+            Print(10);
+            Print(0.123);
+            Print("SUM = ", 123.4);
+        }
+
+        private static void Print(double x)
+        {
+            Console.WriteLine(x);
+        }
+
+        private static void Print(string s, double x) 
+        {
+            Console.WriteLine(s+ x);
+        }
+
+        private static void Print(int x) 
+        {
+            Console.WriteLine(x);
+        }
+
+
+    }
+
+
+
+
+}
