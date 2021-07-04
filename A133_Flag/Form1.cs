26ebf8553af81e23500c866a73d073b2caa007a4
Minbumm <68464294+minbumm@users.noreply.github.com>
2021-07-04 18:51:47 +0900

    A133

---
+++ A133_Flag/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A133_Flag
+{
+    public partial class Form1 : Form
+    {
+        private bool flags; //디폴트는 false
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (flags == false)
+            {
+                label1.Text = "Hello Wolrd!";
+                flags = true;
+            }
+            else 
+            {
+                label1.Text = "";
+                flags = false;
+            }
+        }
+    }
+}
