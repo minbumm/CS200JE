5a0498a6b442fe8d4992a8bbdf598e1df703d32b
minbumm <68464294+minbumm@users.noreply.github.com>
2021-07-05 09:48:39 +0900

    A135

---
+++ A135_RadioButton/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace A135_RadioButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rbOthers = new System.Windows.Forms.RadioButton();
+            this.rbJapen = new System.Windows.Forms.RadioButton();
+            this.rbChina = new System.Windows.Forms.RadioButton();
+            this.rbKorea = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.rbOthers);
+            this.panel1.Controls.Add(this.rbJapen);
+            this.panel1.Controls.Add(this.rbChina);
+            this.panel1.Controls.Add(this.rbKorea);
+            this.panel1.Location = new System.Drawing.Point(78, 89);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(144, 164);
+            this.panel1.TabIndex = 0;
+            // 
+            // rbOthers
+            // 
+            this.rbOthers.AutoSize = true;
+            this.rbOthers.Location = new System.Drawing.Point(27, 117);
+            this.rbOthers.Name = "rbOthers";
+            this.rbOthers.Size = new System.Drawing.Size(91, 16);
+            this.rbOthers.TabIndex = 0;
+            this.rbOthers.TabStop = true;
+            this.rbOthers.Text = "그 외의 국가";
+            this.rbOthers.UseVisualStyleBackColor = true;
+            // 
+            // rbJapen
+            // 
+            this.rbJapen.AutoSize = true;
+            this.rbJapen.Location = new System.Drawing.Point(27, 88);
+            this.rbJapen.Name = "rbJapen";
+            this.rbJapen.Size = new System.Drawing.Size(47, 16);
+            this.rbJapen.TabIndex = 0;
+            this.rbJapen.TabStop = true;
+            this.rbJapen.Text = "일본";
+            this.rbJapen.UseVisualStyleBackColor = true;
+            // 
+            // rbChina
+            // 
+            this.rbChina.AutoSize = true;
+            this.rbChina.Location = new System.Drawing.Point(27, 59);
+            this.rbChina.Name = "rbChina";
+            this.rbChina.Size = new System.Drawing.Size(47, 16);
+            this.rbChina.TabIndex = 0;
+            this.rbChina.TabStop = true;
+            this.rbChina.Text = "중국";
+            this.rbChina.UseVisualStyleBackColor = true;
+            // 
+            // rbKorea
+            // 
+            this.rbKorea.AutoSize = true;
+            this.rbKorea.Location = new System.Drawing.Point(27, 30);
+            this.rbKorea.Name = "rbKorea";
+            this.rbKorea.Size = new System.Drawing.Size(71, 16);
+            this.rbKorea.TabIndex = 0;
+            this.rbKorea.TabStop = true;
+            this.rbKorea.Text = "대한민국";
+            this.rbKorea.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.rbFemale);
+            this.panel2.Controls.Add(this.rbMale);
+            this.panel2.Location = new System.Drawing.Point(254, 89);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 63);
+            this.panel2.TabIndex = 1;
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(107, 19);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(47, 16);
+            this.rbFemale.TabIndex = 0;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "여자";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            this.rbFemale.CheckedChanged += new System.EventHandler(this.rbFemale_CheckedChanged);
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(9, 19);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(47, 16);
+            this.rbMale.TabIndex = 0;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "남자";
+            this.rbMale.UseVisualStyleBackColor = true;
+            this.rbMale.CheckedChanged += new System.EventHandler(this.rbMale_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(257, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "성별";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "국적";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "제출";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rbOthers;
+        private System.Windows.Forms.RadioButton rbJapen;
+        private System.Windows.Forms.RadioButton rbChina;
+        private System.Windows.Forms.RadioButton rbKorea;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ A135_RadioButton/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A135_RadioButton
+{
+    public partial class Form1 : Form
+    {
+        private RadioButton checkRB;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            if (rbKorea.Checked)
+            {
+                result += "국적: 대한민국\n";
+            }
+            else if (rbChina.Checked)
+            {
+                result += "국적: 중국\n";
+            }
+            else if (rbJapen.Checked)
+            {
+                result += "국적: 일본\n";
+            }
+            else if (rbOthers.Checked)
+            {
+                result += "국적:  그 외의 국가\n";
+            }
+
+            if (checkRB == rbMale)
+            {
+                result += "성별: 남성";
+            }
+            else if (checkRB == rbFemale)
+            {
+                result += "성별: 여성";
+                
+            }
+
+            MessageBox.Show(result, "Result");
+        }
+
+        private void rbMale_CheckedChanged(object sender, EventArgs e)
+        {
+            checkRB = rbMale;
+        }
+
+        private void rbFemale_CheckedChanged(object sender, EventArgs e)
+        {
+            checkRB = rbFemale;
+        }
+    }
+}
